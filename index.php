e4939fbe029a925e1c73d5bc82aaf36f8f803c1b
Darkidd77 <m.almahdi77@yahoo.com>
2025-01-23 22:20:50 +0000

    landing page

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+include "includes/header.html"; ?>
+
+<div class="grid-container">
+
+<h1>Welcome to Tradesmen Rater</h1>
+<p>
+Welcome to our platform, where finding reliable tradesmen is simple and hassle-free. We connect you with skilled professionals across various trades, allowing you to search, compare ratings, and book services with ease. Whether you're in need of plumbing, electrical work, carpentry, or any other trade, we make it easier than ever to find trusted experts who get the job done right. Your home improvement projects just got a whole lot simpler!
+</p>
+<img src="img/innovation.jpg" />
+
+</div>
+
+<?php include "includes/footer.html";
+?>
